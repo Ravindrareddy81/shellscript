11b430b871c2b893ad5810588c68c3f07c63ec49
ravindrareddy81 <ravindrareddy375@gmail.com>
2025-01-05 22:11:31 +0530

    script added

--- basicshellscript.sh
+++ basicshellscript.sh
@@ -1,8 +1,7 @@
-#! bin/bash
+#! /bin/bash
 number1=$1
 number2=$2
-
-timestamp=$(date)
-echo "script executed at:$timestamp"
 sum=$(($number1+$number2))
-echo "Sum of two numbers: $sum"
+echo "sum of two numbers:$sum"
+timestamp=$(date)
+echo "current date is:$timestamp"
