50acdbb628a289f7773aff7372781de672b37200
ravindrareddy81 <ravindrareddy375@gmail.com>
2025-01-06 14:00:42 +0530

    sript added

--- installscript.sh
+++ installscript.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
 userid=$(id -u)
 
-if[ $userid -ne 0];
+if[ $userid -ne 0 ];
 then
 echo "error:you must have sudo access to execute"
 exit 1
 fi
 dnf list installed mysql
-if[ $? -ne 0];
+if[ $? -ne 0 ];
 then
 dnf install mysql -y
-   if[ $? -ne 0];
+   if[ $? -ne 0 ];
    then
      echo "installing mysql is failure"
       exit 1
@@ -22,10 +22,10 @@ else
 fi
 
 dnf list installed git
-if[ $? -ne 0];
+if[ $? -ne 0 ];
 then
 dnf install git -y
-  if[ $? -ne 0];
+  if[ $? -ne 0 ];
    then
      echo "installing git is failure"
       exit 1
