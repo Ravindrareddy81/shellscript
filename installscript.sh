b00348a46e439a7c7e8de2ee6de3a0b5d0870752
ravindrareddy81 <ravindrareddy375@gmail.com>
2025-01-06 14:17:48 +0530

    sript added

--- installscript.sh
+++ installscript.sh
@@ -1,37 +1,20 @@
 #!/bin/bash
 
 userid=$(id -u)
-
-# Check if the script is run as root
-if [ $userid -ne 0 ]; then
-  echo "Error: You must have sudo access to execute this script."
-  exit 1
+if[$userid -ne 0];
+then
+echo "error:you must have sudo access"
+exit 1
 fi
-
-# Check if MySQL is installed
-dnf list installed mysql > /dev/null 2>&1
-if [ $? -ne 0 ]; then
+dnf list installed mysql
+if [$? -ne 0];then
   dnf install mysql -y
-  if [ $? -ne 0 ]; then
-    echo "Installing MySQL failed."
-    exit 1
-  else
-    echo "MySQL installed successfully."
-  fi
+   if[$? -ne 0];then
+      echo "mysql is installed:failure"
+      exit 1
+    else
+       echo "mysql us installed:success"
+    fi
 else
-  echo "MySQL is already installed."
-fi
-
-# Check if Git is installed
-dnf list installed git > /dev/null 2>&1
-if [ $? -ne 0 ]; then
-  dnf install git -y
-  if [ $? -ne 0 ]; then
-    echo "Installing Git failed."
-    exit 1
-  else
-    echo "Git installed successfully."
-  fi
-else
-  echo "Git is already installed."
+echo "mysql is already installed"
 fi
