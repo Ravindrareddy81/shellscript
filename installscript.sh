eb855d7a200b22bfd94d564e9c951bed04546cf6
ravindrareddy81 <ravindrareddy375@gmail.com>
2025-01-07 13:38:15 +0530

    sript added

--- installscript.sh
+++ installscript.sh
@@ -4,6 +4,10 @@ userid=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
+LOGS_FOlDER="var/log/shellscript-logs"
+log_file=$(echo $0 | cut -d "." -f1 )
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 validate()
 {
     if [$1 -ne 0]; then
@@ -12,6 +16,7 @@ validate()
       echo -e "$2...$G success"
     fi
 }
+echo "script started executing at: $TIMESTAMP"&>>$log_file
 
 if [ $userid -ne 0 ]; then
  echo "error:you must have sudo access."
@@ -23,14 +28,14 @@ fi
 #fi
 dnf list installed mysql
 if [$? -ne 0];then
-  dnf install mysql -y
+  dnf install mysql -y &>>$log_file
   validate $? "installing mysql"
 else
 echo -e "mysql is already ... $Y installed"
 fi
 dnf list installed git
 if [$? -ne 0];then
-  dnf install git -y
+  dnf install git -y &>>$log_file
   validate $? "installing git"
 else
 echo -e "git is already ... $Y installed"
