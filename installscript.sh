1ef2f022f316599ba2cf78c58066a384bf37cdd3
ravindrareddy81 <ravindrareddy375@gmail.com>
2025-01-07 13:51:15 +0530

    sript added

--- installscript.sh
+++ installscript.sh
@@ -4,19 +4,20 @@ userid=$(id -u)
 R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
-LOGS_FOlDER="var/log/shellscript-logs"
+LOGS_FOLDER="/var/log/shellscript-logs"
 log_file=$(echo $0 | cut -d "." -f1 )
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP.log"
 validate()
 {
-    if [$1 -ne 0]; then
+    if [ $1 -ne 0 ]; then
       echo -e "$2...$R failure"
+      exit 1
     else
       echo -e "$2...$G success"
     fi
 }
-echo "script started executing at: $TIMESTAMP"&>>$log_file
+echo "script started executing at: $TIMESTAMP" &>>$log_file
 
 if [ $userid -ne 0 ]; then
  echo "error:you must have sudo access."
@@ -26,8 +27,8 @@ fi
  # echo "Error: You must have sudo access to execute this script."
   #exit 1
 #fi
-dnf list installed mysql
-if [$? -ne 0];then
+dnf list installed mysql &>>$log_file
+if [ $? -ne 0 ];then
   dnf install mysql -y &>>$log_file
   validate $? "installing mysql"
 else
@@ -38,5 +39,5 @@ if [$? -ne 0];then
   dnf install git -y &>>$log_file
   validate $? "installing git"
 else
-echo -e "git is already ... $Y installed"
+ echo -e "git is already ... $Y installed"
 fi
