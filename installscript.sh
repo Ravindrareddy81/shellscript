12194b927166cd7e8a20c8ce31dc5cc32f498c16
ravindrareddy81 <ravindrareddy375@gmail.com>
2025-01-07 11:54:19 +0530

    sript added

--- installscript.sh
+++ installscript.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 userid=$(id -u)
-
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
 validate()
 {
     if [$1 -ne 0]; then
-      echo "$2:failure"
+      echo "$2...$R failure"
     else
-      echo "$2:success"
+      echo "$2...$G success"
     fi
 }
 
@@ -20,16 +22,16 @@ fi
   #exit 1
 #fi
 dnf list installed mysql
- if [$? -ne 0];then
+if [$? -ne 0];then
   dnf install mysql -y
   validate $? "installing mysql"
 else
-echo "mysql is already installed"
+echo "mysql is already ... $Y installed"
 fi
 dnf list installed git
 if [$? -ne 0];then
   dnf install git -y
   validate $? "installing git"
 else
-echo "git is already installed"
+echo "git is already ... $Y installed"
 fi
