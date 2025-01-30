95896e16fea4f52aa4f84f808aeca9a28e74ad75
ravindrareddy81 <ravindrareddy375@gmail.com>
2025-01-30 13:53:28 +0530

    log-scriptadded

--- specialvar.sh
+++ specialvar.sh
@@ -8,3 +8,23 @@ echo "home directory of cureent user:$home"
 echo "process id of current script:$$"
 sleep 60 &
 echo "process id of last command in background:$!"
+
+function ravi(){
+    echo "hello,$1"
+}
+
+echo"enter the file"
+read source_file
+if [! -f "$source_file"];then
+echo "error:the "source_file'doesn't exist"
+exit 1
+fi
+
+echo"enter the file"
+read source_file
+if [! -f "$source_file"];then
+echo "error:the "source_file 'doesn,t exist"
+exit 1
+fi
+backup_file ="$(source_file)_backup_$(date+ '%Y%m%d_%H%M%S')"
+cp "$source_file" "$backup_file"
